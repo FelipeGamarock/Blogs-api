dd25f3e00180f97c032f25389039de25e46c6bb6
Felipe Gama Danta <epilefgama2@gmail.com>
2022-07-26 15:13:05 -0300

    feat: Inicio req 08, rota post Category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -3,6 +3,19 @@ const TokenMiddleware = require('../middlewares/tokenMiddleware');
 
 const categoryController = {
 
+  async add(req, res) {
+    const token = req.headers.authorization;
+    if (!token) {
+      const err = new Error('Token not found');
+      err.name = 'UnauthorizedError';
+      throw err;
+    }
+    await TokenMiddleware.verifyToken(token);
+    const data = await categoryService.validateBodyAdd(req.body);
+    const category = await categoryService.add(data);
+    return res.status(201).json({ category });
+  },
+
   async list(req, res) {
     const token = req.headers.authorization;
     if (!token) {
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,6 +1,20 @@
+const Joi = require('joi');
 const models = require('../database/models');
 
 const categoryService = {
+  
+  async validateBodyAdd(unknown) {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+    });
+    const result = await schema.validateAsync(unknown);
+    return result;
+  },
+
+  async add(body) {
+    const user = await models.Category.create(body, { raw: true });
+    return user;
+  },
 
   async list() {
     const categories = await models.Category.findAll();
